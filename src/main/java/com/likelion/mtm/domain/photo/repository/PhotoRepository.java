196bd83e1785de8421cc9653aa13652cec6abb1d
최정훈 <haonkr@gmail.com>
2026-08-11 05:03:32 +0900

    [Feat/#6] Feat: 원본 사진 리포지토리 구현

---
+++ src/main/java/com/likelion/mtm/domain/photo/repository/PhotoRepository.java
@@ -0,0 +1,20 @@
+package com.likelion.mtm.domain.photo.repository;
+
+import com.likelion.mtm.domain.photo.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * 원본 사진의 데이터베이스 접근을 담당하는 리포지토리.
+ */
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    /**
+     * 특정 회원의 사진첩을 최신 업로드 순으로 조회한다.
+     *
+     * @param memberId 회원 식별자
+     * @return 해당 회원이 업로드한 원본 사진 목록
+     */
+    List<Photo> findAllByMemberIdOrderByCreatedAtDesc(Long memberId);
+}
